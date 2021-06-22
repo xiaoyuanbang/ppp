c0f65c1c99d9c07267ff4e10aee4916d3f46feb9
wenc <939949243@qq.com>
2021-06-22 18:13:45 +0800

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,14 @@
+public class Application{
+	public static void main(String[] args){
+                //云端内容变化
+// pull =fetch + merge
+<<<<<<< HEAD
+//我是开发人员，虽然我是实习生，但是我git很6哦git 
+///1111
+=======
+//我是项目经理，我改了一点内容
+//222222222
+>>>>>>> 656dd2c4f8a945280ece1fd2d9a2f07acc995450
+		System.out.println("Application");
+	}
+}
